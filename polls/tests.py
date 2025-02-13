85af624c9bd339bf318a358c5991562aa4bc1802
Aunarky <aunarky@tn.productions>
2025-02-13 22:58:49 +0000

    stub

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.http import Http404
+from django.shortcuts import render
+from .models import Question 
 
-# Create your tests here.
+def detail(request, question_id):
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request, 'polls/detail.html', {'question': question})
