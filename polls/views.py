8b99493a6502ea4a69b19440d0a50e4490c353bb
Aunarky <aunarky@tn.productions>
2025-02-14 00:42:59 +0000

    peak

--- polls/views.py
+++ polls/views.py
@@ -2,14 +2,15 @@ from django.http import HttpResponseRedirect, HttpResponse
 from .models import Choice, Question 
 from django.shortcuts import get_object_or_404, render 
 from django.urls import reverse
+from django.utils import timezone
 
 def index(request):
-    latest_questions = Question.objects.order_by('-pub_date')[:3]
+    latest_questions = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:3] 
     context = {'latest_question_list': latest_questions} 
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question.objects.filter(pub_date__lte=timezone.now()), pk=question_id)
     return render(request, 'polls/detail.html', {'question': question}) 
 
 def results(request, question_id):
